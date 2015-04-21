5e5afac6914098301ad3c5d2bf90b11a475d47b1
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2015-04-21 13:07:47 +0200

    Simplest possible implementation with better usage message.

--- src/main/java/dk/statsbiblioteket/cdrip/robot/Main.java
+++ src/main/java/dk/statsbiblioteket/cdrip/robot/Main.java
@@ -14,6 +14,9 @@ import java.time.LocalDateTime;
  */
 public class Main {
 
+	private static final String PATTERN1 = "'CD-Inspector_'yyyy-MM-dd_HH-mm'.log'";
+	private static final String PATTERN2 = "'Result_'yyyy-MM-dd_HH-mm'.log'";
+
 	public static void main(String[] args) throws FileNotFoundException {
 		if (args.length < 1) {
 			throw new IllegalArgumentException("usage: jobfile-path");
@@ -22,21 +25,17 @@ public class Main {
 
 		LocalDateTime startTime = LocalDateTime.now();
 
-		String filename1 = startTime
-				.format(ofPattern("'CD-Inspector_'yyyy-MM-dd_HH-mm'.log'"));
+		String filename1 = startTime.format(ofPattern(PATTERN1));
 		File file1 = new File(filename1);
 		PrintWriter pw1 = new PrintWriter(file1);
 		pw1.println("CD-Inspector Version: 2. 0. 1. 4  Logfile");
 		pw1.println("11/04   14:08   " + jobfile + " loaded successful");
 		pw1.close();
 
-		String filename2 = startTime
-				.format(ofPattern("'Result_'yyyy-MM-dd_HH-mm'.log'"));
+		String filename2 = startTime.format(ofPattern(PATTERN2));
 		File file2 = new File(filename2);
 		PrintWriter pw2 = new PrintWriter(file2);
 		pw2.println("CD-Inspector Version: 2. 0. 1. 4  Logfile");
 		pw2.close();
-
 	}
-
 }
