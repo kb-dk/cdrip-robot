f07b5b72c6cf52b08ac1ba1adc0c52f5db0b93ba
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2015-04-21 12:08:09 +0200

    Simplest possible implementation.

---
+++ src/main/java/dk/statsbiblioteket/cdrip/robot/Main.java
@@ -0,0 +1,42 @@
+package dk.statsbiblioteket.cdrip.robot;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+/**
+ * Simplest possible implementation. Provide the jobfile path string as args[0].
+ * Writes files to current directory
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		if (args.length < 1) {
+			throw new IllegalArgumentException("usage: jobfile-path");
+		}
+		String jobfile = args[0];
+
+		LocalDateTime startTime = LocalDateTime.now();
+
+		String filename1 = startTime
+				.format(ofPattern("'CD-Inspector_'yyyy-MM-dd_HH-mm'.log'"));
+		File file1 = new File(filename1);
+		PrintWriter pw1 = new PrintWriter(file1);
+		pw1.println("CD-Inspector Version: 2. 0. 1. 4  Logfile");
+		pw1.println("11/04   14:08   " + jobfile + " loaded successful");
+		pw1.close();
+
+		String filename2 = startTime
+				.format(ofPattern("'Result_'yyyy-MM-dd_HH-mm'.log'"));
+		File file2 = new File(filename2);
+		PrintWriter pw2 = new PrintWriter(file2);
+		pw2.println("CD-Inspector Version: 2. 0. 1. 4  Logfile");
+		pw2.close();
+
+	}
+
+}
